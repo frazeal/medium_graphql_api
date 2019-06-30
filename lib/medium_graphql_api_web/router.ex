b5f2ff1b51f4ff0057a371d4b858f4b1b95ca2f8
frazeal <aaalves.francisco@gmail.com>
2019-06-30 19:24:16 -0300

    setup schema file

--- lib/medium_graphql_api_web/router.ex
+++ lib/medium_graphql_api_web/router.ex
@@ -2,10 +2,16 @@ defmodule MediumGraphqlApiWeb.Router do
   use MediumGraphqlApiWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", MediumGraphqlApiWeb do
-    pipe_through :api
+    pipe_through(:api)
+
+    if Mix.env() == :dev do
+      forward("/graphiql", Absinthe.Plug.GraphiQL, schema: MediumGraphqlApiWeb.Schema)
+    else
+      forward("/graphql", Absinthe.Plug, schema: MediumGraphqlApiWeb.Schema)
+    end
   end
 end
